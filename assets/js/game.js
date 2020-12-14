f7e4f2838b6610a3b5fa04ab1b42136eb17c43e2
Austin Dudzic <austin.dudzic@gmail.com>
2020-12-13 23:33:18 -0500

    variables and functions

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,17 @@
-window.alert("This is a window alert! Javascript is running!")
+// this creates a function called "fight"
+
+function fight() {
+    window.alert("The fight has begun!");
+}
+
+var playername = window.prompt("What is your robot's name?")
+// note the lack of quatation marks around playername
+//what is this
+console.log(playername);
+
+console.log("This log is a string, good for leaving youself a message");
+// this will do math and log 20
+console.log(10 + 10);
+//WHat is this;
+console.log("our robots name is " + playername)
+// fight();
